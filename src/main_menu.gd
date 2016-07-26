78bb3dec5d616760fb6b9d12d4db368c05424e85
Elia Argentieri <elia.argentieri@openmailbox.org>
2016-07-27 01:40:36 +0200

    Scn to Tscn (binary format to more debugable text format) files.

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 func _on_singleplayer_pressed():
 	# ToDo: select characters
 	print("Load game")
-	get_tree().change_scene("res://scene/game.scn")
+	get_tree().change_scene("res://scene/game.tscn")
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
