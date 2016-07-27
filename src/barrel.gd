c7b90e2e0ae1781d32f0355afaf28d212e183d32
Elia Argentieri <elia.argentieri@openmailbox.org>
2016-07-28 01:01:24 +0200

    Improved barrel

--- src/barrel.gd
+++ src/barrel.gd
@@ -4,6 +4,9 @@ var timeout = 5
 var lifetime = null
 var voice = null
 
+onready var light_node = get_node("explosion/light")
+onready var particle_node = get_node("explosion/particles")
+
 func _ready():
 	set_process(true)
 
@@ -15,12 +18,14 @@ func _process(delta):
 			set_process(false)
 			queue_free()
 	elif voice != null and not get_node("explosion/sound").is_voice_active(voice):
-		get_node("explosion/particles").set_emitting(false)
-		lifetime = get_node("explosion/particles").get_variable(Particles.VAR_LIFETIME)
+		particle_node.set_emitting(false)
+		light_node.set_enabled(false)
+		lifetime = particle_node.get_variable(Particles.VAR_LIFETIME)
 	elif timeout < 0 and voice == null:
 		voice = get_node("explosion/sound").play("explosion")
-		get_node("explosion/particles").set_emitting(true)
+		particle_node.set_emitting(true)
 		get_node("mesh").hide()
+		light_node.set_enabled(true)
 		for body in get_node("explosion").get_overlapping_bodies():
 			if body extends preload("res://src/entity.gd"):
 				body.damage(20, 0.1)
