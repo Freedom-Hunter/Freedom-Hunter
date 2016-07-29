afd1bfa3c6ad7d854e612180cc9d8cb62d8f4535
Elia Argentieri <elia.argentieri@openmailbox.org>
2016-07-29 02:29:06 +0200

    Usable barrel

---
+++ src/items/barrel.gd
@@ -0,0 +1,12 @@
+extends "item.gd"
+
+var scene = preload("res://scene/items/barrel.tscn")
+
+func init(p, i, n, q):
+	.init(p, i, n, q, true)
+
+func effect():
+	var drop = player.get_node("drop_item").get_global_transform()
+	var barrel = scene.instance()
+	barrel.set_global_transform(drop)
+	player.get_parent().add_child(barrel)
