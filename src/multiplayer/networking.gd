3cf0b16a0ab8f0a2e6309e5957699102d1d1809b
Elia Argentieri <elia.argentieri@openmailbox.org>
2016-08-08 23:43:07 +0200

    Fix some client issues

--- src/multiplayer/networking.gd
+++ src/multiplayer/networking.gd
@@ -98,21 +98,18 @@ func server_send_to_player(pckt, player):
 
 func process_client(pckt, delta):
 	if pckt.command == CMD_SC_MOVE:
-		if spawn_node.has_node(pckt.args.player):
-			spawn_node.get_node(pckt.args.player).set_global_transform(pckt.args.transform)
+		if pckt.args.player in players.keys():
+			players[pckt.args.player].set_global_transform(pckt.args.transform)
 	elif pckt.command == CMD_SC_DAMAGE:
-		if spawn_node.has_node(pckt.args.player):
-			var player = spawn_node.get_node(pckt.args.player + "/body")
-			player.damage(pckt.args.damage, pckt.args.regenerable, false)
+		if pckt.args.player in players.keys():
+			players[pckt.args.player].damage(pckt.args.damage, pckt.args.regenerable, false)
 	elif pckt.command == CMD_SC_ATTACK:
-		if spawn_node.has_node(pckt.args.player):
-			var player = spawn_node.get_node(pckt.args.player + "/body")
-			player.weapon_node.set_rotation_deg(Vector3(pckt.args.rot, 0, 0))
+		if pckt.args.player in players.keys():
+			players[pckt.args.player].weapon_node.set_rotation_deg(Vector3(pckt.args.rot, 0, 0))
 	elif pckt.command == CMD_SC_DIE:
-		if spawn_node.has_node(pckt.args):
-			var player = spawn_node.get_node(pckt.args + "/body")
-			if player.hp > 0:
-				player.die(false)
+		if pckt.args.player in players.keys():
+			if players[pckt.args.player].hp > 0:
+				players[pckt.args.player].die(false)
 	elif pckt.command == CMD_SC_USE:
 		players[pckt.args.player].items[pckt.args.item].use()
 	elif pckt.command == CMD_SC_USERNAME_IN_USE:
