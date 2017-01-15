7254cda62ab397c7ae9b8a2b56096f2da6780510
Elia Argentieri <elia.argentieri@openmailbox.org>
2017-01-15 22:47:22 +0100

    Fix whetstone

--- src/items/whetstone.gd
+++ src/items/whetstone.gd
@@ -6,8 +6,8 @@ func _init(pla, ico, nam, qua, sha).(pla, ico, nam, qua, 10, true, 50):
 	sharp = sha
 
 func effect():
-	if player.equipment.sword != null and not player.equipment.sword.is_sharpened():
-		player.equipment.sword.sharpen(sharp)
+	if player.equipment.weapon != null and not player.equipment.weapon.is_sharpened():
+		player.equipment.weapon.sharpen(sharp)
 		player.animation_node.play("whetstone")
 		player.audio_node.play("whetstone")
 		return true
