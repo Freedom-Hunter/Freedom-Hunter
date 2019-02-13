8e45bb90904407674cdd2e79f65c41854b115c9e
Elia Argentieri <elia@elinvention.ovh>
2019-02-14 00:21:09 +0100

    Improve status bar

--- src/interface/status.gd
+++ src/interface/status.gd
@@ -3,18 +3,18 @@ extends VBoxContainer
 onready var global = get_node("/root/global")
 
 func _process(delta):
-	$hp.max_value = global.local_player.hp_max
-	$hp.rect_size.x = 300 * global.local_player.hp_max / 50
-	$hp.value = global.local_player.hp
+	$hp/bar.max_value = global.local_player.hp_max
+	$hp/bar.rect_size.x = 300 * global.local_player.hp_max / 50
+	$hp/bar.value = global.local_player.hp
 
-	$hp/red_hp.max_value = global.local_player.hp_max
-	$hp/red_hp.rect_size.x = $hp.rect_size.x
-	$hp/red_hp.value = global.local_player.hp_regenerable
+	$hp/bar/red.max_value = global.local_player.hp_max
+	$hp/bar/red.rect_size.x = $hp.rect_size.x
+	$hp/bar/red.value = global.local_player.hp_regenerable
 
-	$stamina.max_value = global.local_player.stamina_max
-	$stamina.value = global.local_player.stamina
-	$stamina.rect_size.x = 300 * global.local_player.stamina_max / 50
+	$stamina/bar.max_value = global.local_player.stamina_max
+	$stamina/bar.value = global.local_player.stamina
+	$stamina/bar.rect_size.x = 300 * global.local_player.stamina_max / 50
 
-	$hp_label.text = "%d/%d" % [global.local_player.hp, global.local_player.hp_max]
-	$stamina_label.text = "%d/%d" % [global.local_player.stamina, global.local_player.stamina_max]
+	$hp/label.text = "%d/%d" % [global.local_player.hp, global.local_player.hp_max]
+	$stamina/label.text = "%d/%d" % [global.local_player.stamina, global.local_player.stamina_max]
 
