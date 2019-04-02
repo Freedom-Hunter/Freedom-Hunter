46271d4f88b9b9e87349f8d31423bb812bf968a2
Elia Argentieri <elia@elinvention.ovh>
2019-04-02 03:39:58 +0200

    Usual .import spam

--- data/models/player/post-import.gd
+++ data/models/player/post-import.gd
@@ -4,6 +4,7 @@ extends EditorScenePostImport
 func post_import(scene):
 	var player = scene.get_node("AnimationPlayer")
 	player.set_name("entity_animation")
+	player.autoplay = "idle"
 
 	var add_audio = {"death": "head", "dodge": "head", "whetstone": "weapon_R"}
 	for anim_name in add_audio:
@@ -11,5 +12,9 @@ func post_import(scene):
 		var track = anim.add_track(Animation.TYPE_AUDIO)
 		anim.track_set_path(track, "Armature/Skeleton/%s/audio" % add_audio[anim_name])
 		anim.audio_track_insert_key(track, 0, load("res://data/sounds/%s.wav" % anim_name))
+
+	for anim_name in ["idle", "run", "walk"]:
+		var anim = player.get_animation(anim_name)
+		anim.loop = true
 
 	return scene # remember to return the imported scene
