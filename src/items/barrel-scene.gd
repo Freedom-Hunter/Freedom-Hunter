e99391e44306efc1f42262353e80f04c84522547
Elia Argentieri <elia@elinvention.ovh>
2019-04-20 02:29:31 +0200

    Make barrels great again!

--- src/items/barrel-scene.gd
+++ src/items/barrel-scene.gd
@@ -4,24 +4,23 @@ const Entity = preload("res://src/entities/entity.gd")
 
 func _on_timer_timeout():
 	$"explosion/animation".play("explode")
-	var r = $"explosion/radius".get_shape().get_radius()
+	var r = $"explosion/radius".shape.radius
 	for body in $"explosion".get_overlapping_bodies():
 		if body == self:
 			continue
+		var d = body.global_transform.origin - global_transform.origin
 		if body is Entity:
-			var d = body.global_transform.origin - global_transform.origin
-			var dmg = int((r - d.length()) * 5 + 1)
+			var dmg = int((r - d.length()) * 20 + 1)
 			body.damage(dmg, 0.1)
 		elif body.is_in_group("explosive"):
-			var diff = body.get_translation() - get_translation()
-			var speed = (r - diff.length()) * 2
-			var direction = diff.normalized()
-			body.set_linear_velocity(direction * speed)
+			var momentum = d.normalized() * (r - d.length()) * mass
+			body.apply_central_impulse(momentum)
 			if not body.get_node("explosion/animation").is_playing():
-				var body_timer = body.get_node("explosion/timer")
-				body_timer.stop()
-				body_timer.set_wait_time(0.5)
-				body_timer.start()
+				var body_timer: Timer = body.get_node("explosion/timer")
+				if body_timer.time_left > 0.5:
+					body_timer.stop()
+					body_timer.set_wait_time(0.5 - rand_range(0, 0.4))
+					body_timer.start()
 
 func _on_animation_finished(animation):
 	queue_free()
