af7097462ce932dea90a353bfae08997918e8512
Elia Argentieri <elia@elinvention.ovh>
2020-11-30 18:43:54 +0100

    refactor firework

--- src/items/firework-scene.gd
+++ src/items/firework-scene.gd
@@ -1,12 +1,15 @@
 extends RigidBody
 
 
-func _on_animation_finished(animation):
-	queue_free()
-
-
-func fire():
-	linear_velocity = Vector3(0, 60, 0)
+func launch():
+	linear_velocity = Vector3(0, 70, 0)
 	linear_velocity.x += rand_range(-20, 20)
 	linear_velocity.y += rand_range(-20, 20)
 	linear_velocity.z += rand_range(-20, 20)
+	$animation.play("launch")
+	$Timer.wait_time += rand_range(0, 1)
+	$Timer.start()
+	yield($Timer, "timeout")
+	$animation.play("boom")
+	yield($animation, "animation_finished")
+	queue_free()
--- src/items/firework.gd
+++ src/items/firework.gd
@@ -10,7 +10,7 @@ func _init(_name, _icon, _quantity).(_name, _icon, _quantity, 10, 50):
 func effect(_player):
 	var firework = scene.instance()
 	_player.drop_item_on_floor(firework)
-	firework.get_node("animation").play("firework")
+	firework.launch()
 	return true
 
 
